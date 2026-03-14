96bb712d3d79b5b93706e0af385985c63c873f2d
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-03-14 06:28:24 +0600

    signin

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,60 +6,54 @@ import { sendResponse } from "../../shared/sendResponse";
 import status from "http-status";
 import { CookieUtils } from "../../utils/cookie";
 
-const getCurrentUser = catchAsync(async (
-  req: Request,
-  res: Response
-) => {
-    const user = req.user;
-    if (!user) {
-      return res
-        .status(401)
-        .json({ success: false, message: "you are unauthorized" });
-    }
-    const result = await authService.getCurrentUser(user.id);
-    sendResponse(res,{
-      httpStatusCode:status.OK,
-      success:true,
-      message:"retrieve current user successsfully",
-      data:result
-    })
-
+const getCurrentUser = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  if (!user) {
+    return res
+      .status(401)
+      .json({ success: false, message: "you are unauthorized" });
+  }
+  const result = await authService.getCurrentUser(user.id);
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "retrieve current user successsfully",
+    data: result,
+  });
 });
 
-const signoutUser = catchAsync(
-  async (req: Request, res: Response) => {
-      const betterAuthSessionToken = req.cookies["better-auth.session_token"];
-    const user = req.user;
-    if (!user) {
-      return res
-        .status(401)
-        .json({ success: false, message: "you are unauthorized" });
-    }
-    const result = await authService.signoutUser(user.id, betterAuthSessionToken);
-    CookieUtils.clearCookie(res, "accesstoken", {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    });
-    CookieUtils.clearCookie(res, "refreshtoken", {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    });
-    CookieUtils.clearCookie(res, "better-auth.session_token", {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    });
+const signoutUser = catchAsync(async (req: Request, res: Response) => {
+  const betterAuthSessionToken = req.cookies["better-auth.session_token"];
+  const user = req.user;
+  if (!user) {
+    return res
+      .status(401)
+      .json({ success: false, message: "you are unauthorized" });
+  }
+  const result = await authService.signoutUser(user.id, betterAuthSessionToken);
+  CookieUtils.clearCookie(res, "accesstoken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+  CookieUtils.clearCookie(res, "refreshtoken", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+  CookieUtils.clearCookie(res, "better-auth.session_token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 
-    sendResponse(res, {
-      httpStatusCode: status.OK,
-      success: true,
-      message: "User logged out successfully",
-      data: result,
-    });
-  },
-);
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "User logged out successfully",
+    data: result,
+  });
+});
 
 const signup = catchAsync(async (req: Request, res: Response) => {
   const result = await authService.signup(req.body);
@@ -80,14 +74,17 @@ const signup = catchAsync(async (req: Request, res: Response) => {
 });
 
 const signin = catchAsync(async (req: Request, res: Response) => {
-    const result = await authService.signin(req.body);
-    sendResponse(res,{
-      httpStatusCode:status.OK,
-      success:true,
-      message:"user signin successfully",
-      data:result
-    })
- 
+  const result = await authService.signin(req.body);
+  const { accessToken, refreshToken, token } = result;
+  tokenUtils.setAccessTokenCookie(res, accessToken);
+  tokenUtils.setRefreshTokenCookie(res, refreshToken);
+  tokenUtils.setBetterAuthSessionCookie(res, token as string);
+  sendResponse(res, {
+    httpStatusCode: status.OK,
+    success: true,
+    message: "user signin successfully",
+    data: result,
+  });
 });
 export const authController = {
   getCurrentUser,
