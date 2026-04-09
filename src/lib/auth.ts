d384e07571f31cdd5c1df558af2b21e6dea67ca7
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-04-09 16:23:10 +0600

    google configaration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,14 +2,15 @@ import { betterAuth, string } from "better-auth";
 import { prisma } from "./prisma";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { bearer } from "better-auth/plugins";
+import { Role, Status } from "../../generated/prisma/enums";
 
 
 export const auth = betterAuth({
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: process.env.FRONTEND_URL,
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql",
     }),
-    trustedOrigins: [process.env.APP_URL!],
+    trustedOrigins: [process.env.FRONTEND_URL!],
     user: {
         additionalFields: {
             role: {
@@ -52,14 +53,53 @@ export const auth = betterAuth({
         autoSignIn:true,
         // requireEmailVerification: true
     },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7,
+        updateAge: 60 * 60 * 24, 
+        strategy: "jwt",
+    },
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
             accessType: "offline",
             prompt: "select_account consent",
-        },
+            redirectURI:`${process.env.FRONTEND_URL}/api/auth/callback/google`,
+            mapProfileToUser: () => {
+              return {
+                role: Role.Customer,
+                status: Status.activate,
+                emailVerified: true,
+              };
+            },
+          },
     },
+    advanced: {
+        // disableCSRFCheck: true,
+        useSecureCookies: false,
+        cookies: {
+          state: {
+            attributes: {
+              sameSite: "none",
+              secure: true,
+              httpOnly: true,
+              path: "/",
+            },
+          },
+          sessionToken: {
+            attributes: {
+              sameSite: "none",
+              secure: true,
+              httpOnly: true,
+              path: "/",
+            },
+          },
+        },
+      },
+    
+      redirectURLs: {
+        signin: `${process.env.BETTER_AUTH_URL}`,
+      },
 });
 
 
